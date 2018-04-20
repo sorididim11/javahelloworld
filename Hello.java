4054c894b1c5c6a7f941b6ce5d06ed3d3deb23fc
sorididim11 <sorididim11@gmail.com>
2018-04-20 13:51:29 +0900

    refactor groups of host file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 
 public class Hello
 {
-  public static void main( String [] args) 
-  { 
-     System.out.println("Hello");
-  } 
+  public static void main( String [] args)
+  {
+     System.out.println("Hello world!2");
+  }
 }
